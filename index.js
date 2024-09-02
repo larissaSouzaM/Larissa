e8edba7a410da12d5cb017812b6e464a78b673fd
Larissa de Souza Mendes <161725440+larissaSouzaM@users.noreply.github.com>
2024-09-02 13:14:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const nome = "Regina";
+const idade = 12;
+const altura = 1.60;
+const peso = 50;
+const sobrenome = "Gonzaga"
+const imc = peso / (altura * altura);
+
+console.log(`Paciente: ${nome} ${sobrenome} tem idade de ${idade} anos, altura${altura} massa ${peso} e seu imc é ${imc}`)
